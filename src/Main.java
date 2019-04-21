ebf07a22fd708215d59809a3ea18dfbebf63b169
AlvaroMarques <sc.poi458@gmail.com>
2019-04-21 19:13:25 -0300

    csv adicionado

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		DataSetComponent data = new DataSetComponent();
-		data.setDataSource("../../../../db/zombie/zombie-health-spreadsheet-ml-training.csv");        
+		data.setDataSource("../csv/zombie-health-spreadsheet-ml-training.csv");        
 		Paciente p = new Paciente(data);
 		Doutor d = new Doutor(p, data);
 		d.getIllness();
